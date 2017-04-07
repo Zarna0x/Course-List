11255735e5c751722bcef117dab38c4cfa6e5668
Zarna0x <justnightmare123@gmail.com>
2017-04-08 02:00:57 +0400

    change language hook

--- application/hooks/LanguageLoader.php
+++ application/hooks/LanguageLoader.php
@@ -6,6 +6,9 @@ class LanguageLoader
         $ci =& get_instance();
         #parr($ci);
         //$ci->load->helper('language');
+        if (get_lang() == null) {
+          $ci->session->set_userdata('site_lang','eng');
+        }
         $site_lang = $ci->session->userdata('site_lang');
 
         if ($site_lang) {
